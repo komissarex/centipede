a5dd7702138b47530ce7f9d66b441e53c25996a1
komissarex <komissarex@gmail.com>
2012-10-17 02:18:42 +0300

    initial commit

---
+++ centipede.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+
+from flask import Flask
+from flask.templating import render_template
+import time, datetime 
+
+import configuration
+
+centipede = Flask(__name__)
+centipede.config.from_object(configuration.Init)
+
+def time_counter(timedelta):
+    hours, remainder = divmod(timedelta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return '%s ч. %s мин. %s сек.' % (hours, minutes, seconds)
+
+@centipede.route('/')
+def index():
+    start = time.mktime(time.strptime(centipede.config['START_TIME'], '%Y-%m-%d %H:%M:%S'))
+    diff = datetime.datetime.fromtimestamp(start) - datetime.datetime.now()
+    return render_template('index.html', counter = time_counter(diff))
+
+if __name__ == '__main__':
+    centipede.run(host = 'localhost', port = 8080, debug = True)
---
+++ configuration.py
@@ -0,0 +1,10 @@
+class Init:
+    
+    #global settings
+    
+    DEBUG = True
+    LOGGING = True
+    
+    #contest settings
+    
+    START_TIME = '2012-10-17 01:53:59'
